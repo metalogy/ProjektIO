9463d422d896dcc830b939c40ce1692efd9d1c94
metalogy <51051022+metalogy@users.noreply.github.com>
2021-01-10 21:23:06 +0100

    Ostatnie porawki serwera

---
+++ Serwer/BankServerAPM.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.IO;
+
+using System.Linq;
+
+using System.Net;
+
+using System.Net.Sockets;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+
+
+
+
+namespace Serwer
+
+{
+    /// <summary>
+    /// To jest klasa dla serwera bankowego w rozwiązaniu asynchronicznym.
+    /// </summary>
+    public class BankServerAPM : BankServer
+
+    {
+        #region PolaKlasy
+        public delegate void TransmissionDataDelegate(NetworkStream stream);
+        protected bool running;
+
+        public static DataBase db = new DataBase();
+        #endregion
+
+        #region KonstruktoryKlasy
+        public BankServerAPM(IPAddress IP, int port) : base(IP, port)
+        {
+           
+        }
+        #endregion
+
+        #region MetodyKlasy
+        /// <summary>
+        /// Funkcja akceptująca różnych klientów. 
+        /// </summary>
+        protected override void AcceptClient()
+
+        {
+
+            while (true)
+
+            {
+
+                TcpClient client = TcpListener.AcceptTcpClient();
+
+                Stream =client.GetStream();
+
+                TransmissionDataDelegate transmissionDelegate = new TransmissionDataDelegate(BeginDataTransmission);
+
+                transmissionDelegate.BeginInvoke(Stream, TransmissionCallback,client);
+
+            }
+
+        }
+
+
+        /// <summary>
+        /// Funkcja sprzątająca po kliencie. 
+        /// </summary>
+        private void TransmissionCallback(IAsyncResult ar)
+        {
+            TcpClient client = (TcpClient)ar.AsyncState;
+            client.Close();
+            db.Write2DataBase();
+
+        }
+
+        /// <summary>
+        /// Funkcja odpowiedzialna za komunikację pomiędzy serwerem, a klientem.
+        /// </summary>
+        /// <param name="stream">Strumień komunikacjny pomiędzy poszczególnym klientem, a serwerem.</param>
+        protected override void BeginDataTransmission(NetworkStream stream)
+        {
+            CommunicationSupport cs = new CommunicationSupport();
+            byte[] buffer = new byte[Buffer_size];
+
+
+            while (true)
+            {
+                try
+                {
+                    stream.Read(buffer, 0,Buffer_size); 
+                    string communicate = Encoding.ASCII.GetString(buffer);
+                    Array.Clear(buffer, 0, buffer.Length);
+                    string reply = cs.AnalysingCommunicate(communicate);
+                    byte[] msg = Encoding.ASCII.GetBytes(reply);  ///wiadomość zwrotna
+                    stream.Write(msg, 0, msg.Length);
+                }
+                catch (IOException e)
+                {
+                    break;
+                }
+            }
+           
+        }
+
+        /// <summary>
+        /// Funkcja rozpoczynająca działanie serwera.
+        /// </summary>
+        public override void Start()
+
+        {
+            db.ReadDataBase();
+            StartListening();
+
+            //transmission starts within the accept function
+
+            AcceptClient();
+            
+
+        }
+
+        #endregion
+    }
+
+}
---
+++ Serwer/CommunicationSupport.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serwer
+{
+    /// <summary>
+    /// To jest klasa odpowiedzialna za analizę komunikatów.
+    /// </summary>
+    class CommunicationSupport
+    {
+        #region PolaKlasy
+        private string login;
+        #endregion
+
+        #region MetodyKlasy
+
+        /// <summary>
+        /// Funkcja analizująca dany ciąg znaków.
+        /// </summary>
+        /// <param name="stream">Ciąg znaków reprezentujący dany komunikat.</param>
+        /// <returns>Ciąg znaków będący odpowiedzią na komunikat. </returns>
+        public string AnalysingCommunicate(string communicate)
+        {
+            string feedback;
+            string[] components = communicate.Split(':');
+            switch (components[0])
+            {
+                case "1":
+                    if(BankServerAPM.db.CheckPassword(components[1], components[2]))
+                    {
+                        feedback = "1";
+                        this.login = components[1];
+                        return feedback;
+                    }
+                    else
+                    {
+                        feedback = "0";
+                        return feedback;
+                    }
+                case "2":
+                    feedback = BankServerAPM.db.ReadBalance(this.login).ToString();
+                    return feedback;
+                case "3":
+                    BankServerAPM.db.AddBalance(this.login, (-1*Int32.Parse(components[1])));
+                    feedback = "1";
+                    return feedback;
+                case "4":
+                    BankServerAPM.db.AddBalance(this.login, (1 * Int32.Parse(components[1])));
+                    feedback = "1";
+                    return feedback;
+                case "5":
+                    try
+                    {
+                        if (BankServerAPM.db.TransferMoney(this.login, components[1], Int32.Parse(components[2])))
+                        {
+                            feedback = "1";
+                        }
+                        else
+                        {
+                            feedback = "0";
+                        }
+                        return feedback;
+                    }
+                    catch(Exception e)
+                    {
+                        feedback = "0";
+                        return feedback;
+                    }
+                   
+                case "6":
+                    try
+                    {
+                        feedback=BankServerAPM.db.downloadAccounts();
+                    }
+                    catch (Exception e)
+                    {
+                        feedback = "0";
+                        return feedback;
+                    }
+                    return feedback;
+
+                case "7":
+                    try
+                    {
+                        BankServerAPM.db.AddEntry(components[1], components[2], components[3], components[4]);
+                    }
+                    catch (Exception e)
+                    {
+                        feedback = "0";
+                        return feedback;
+                    }
+                    feedback = "1";
+                    return feedback;
+                case "8":
+                    try
+                    {
+                        BankServerAPM.db.DeleteEntry(components[1]);
+                        this.login = "";
+                        feedback = "1";
+                        return feedback;
+                    }
+                    catch (Exception e)
+                    {
+                        feedback = "0";
+                        return feedback;
+                    }
+                   
+                case "9":
+                    if (BankServerAPM.db.adminLogin(components[1], components[2]))
+                    {
+                        feedback = "1";
+                        this.login = components[1];
+                        return feedback;
+                    }
+                    else
+                    {
+                        feedback = "0";
+                        return feedback;
+                    }
+                case "10":
+                    String data = BankServerAPM.db.downloadDatas(components[1]);
+                    if (data!=null)
+                    {
+                        feedback = data;
+                        return feedback;
+                    }
+                    else
+                    {
+                        feedback = "0";
+                        return feedback;
+                    }
+                case "11":
+                    try
+                    {
+                        BankServerAPM.db.ChangeUserData(components[1], components[2], components[3], components[4], components[5], Int32.Parse(components[6]));
+                    }
+                    catch (Exception e)
+                    {
+                        feedback = "0";
+                        return feedback;
+                    }
+                    feedback = "1";
+                    return feedback;
+
+                default:
+                    feedback = "Error";
+                    return feedback;
+            }
+        }
+        #endregion 
+
+    }
+}
